6329f05f26743db07305f8226a13d9e58d16f989
TormentedTraveler <114169086+TormentedTraveler@users.noreply.github.com>
2024-09-13 13:01:31 +0500

    Create lesson1.f95

---
+++ lesson1.f95
@@ -0,0 +1,36 @@
+program machine_epsilon_32bit_precision
+    implicit none
+    real :: epsilon, n
+
+    epsilon = 1.0
+    n = 0.0
+
+    do while (1.0 + epsilon /= 1.0)
+        epsilon = epsilon / 10.0
+        n = n + 1
+        print *, epsilon, n
+    end do
+
+    print *, 'Machine epsilon for single precision is: ', epsilon
+    print *, 'Number of iteration it took is: ', n
+end program machine_epsilon_32bit_precision
+
+! program machine_epsilon_64bit_precision
+!     implicit none
+!     double precision :: epsilon, n
+
+!     epsilon = 1.0d0
+!     n = 0.0d0
+
+!     do while (1.0d0 + epsilon > 1.0d0)
+!         epsilon = epsilon / 10.0d0
+!         n = n + 1.0d0
+!         print *, epsilon, n
+!     end do
+
+!     print *, 'Machine epsilon for double precision is: ', epsilon
+!     print *, 'Number of iteration it took is: ', n
+! end program machine_epsilon_64bit_precision
+
+
+
